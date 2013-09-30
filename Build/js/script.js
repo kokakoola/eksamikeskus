75334ed75d36324f87d019158eeb2a30a7b7d230
mare hunt <mare.hunt@netgroup.ee>
2013-09-30 22:43:55 +0300

    with angular js

---
+++ Build/js/script.js
@@ -0,0 +1,12 @@
+
+
+function EmployeeController($scope) {
+  $scope.department = 'Engineering';
+  $scope.employee = {
+    name: 'Joe the Manager',
+    reports: [
+      {name: 'John Smith'},
+      {name: 'Mary Run'}
+    ]
+  };
+}
