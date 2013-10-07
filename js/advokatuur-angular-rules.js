bc0714dce3b681e2d08c5f494d93647b8c90ec60
mare hunt <mare.hunt@netgroup.ee>
2013-10-07 18:51:48 +0300

    finished test part

--- js/advokatuur-angular-rules.js
+++ js/advokatuur-angular-rules.js
@@ -2,7 +2,120 @@
 function QuestionListCtrl($scope) {
   $scope.field = "Eraõigus ja tsiviilkohtumenetlus"
   $scope.tests = [ 
-{
+{   
+    "field": "Eraõigus ja tsiviilkohtumenetlus",
+    "question": "Käsipant, samuti õiguste pant lõpeb",
+    "answers":  {
+      "a":"pandiga tagatud nõude loovutamisel",
+      "b":"pandi registrist kustutamisel",
+      "c":"pandi eseme üleandmisel",
+      "d":"kui pandipidaja teatab pantijale kirjalikult pandi vabastamisest"
+      }
+     },
+   {
+    "question": "Nõude aegumine peatub mh",
+    "answers": {
+      "a":"hagiavalduse esitamisega",
+      "b":"nõude tunnustamisega kohustatud isiku poolt",
+      "c":"läbirääkimiste pidamisega läbirääkimiste toimumise ajaks",
+      "d":"nõude loovutamisega"
+      }
+   },
+   {
+    "question": "Tsiviilasjades võib maakohtu otsuse peale apellatsioonkaebuse esitada",
+    "answers": { 
+        "a":"10 päeva jooksul alates otsuse apellandile kättetoimetamisest",
+        "b":"15 päeva jooksul alates otsuse apellandile kättetoimetamisest",
+        "c":"20 päeva jooksul alates otsuse apellandile kättetoimetamisest",
+        "d":"30 päeva jooksul alates otsuse apelllandile kättetoimetamisest"
+      },
+    "note": {
+      "a":"nok",
+      "b":"ok",
+      "c":0,
+      "d":0
+    }
+   },
+    {
+    "question": "Müügilepingu sõlmimisega",
+    "answers": {
+      "a": "tekib ostjal õigus nõuda müügieseme omandi ülekandmist",
+      "b": "läheb müügieseme omand üle ostjale juhul, kui ta on müügihinna täielikult tasunud",
+      "c": "läheb müügieseme omand üle ostjale, välja arvatud juhul, kui on kokku lepitud omandireservatsioonis",
+      "d": "täidab ostja oma ostuhinna tasumise kohustuse"
+    }
+   },{
+    "question": "Käsipant, samuti õiguste pant lõpeb",
+    "answers":  {
+      "a":"pandiga tagatud nõude loovutamisel",
+      "b":"pandi registrist kustutamisel",
+      "c":"pandi eseme üleandmisel",
+      "d":"kui pandipidaja teatab pantijale kirjalikult pandi vabastamisest"
+      }
+     },
+   {
+    "question": "Nõude aegumine peatub mh",
+    "answers": {
+      "a":"hagiavalduse esitamisega",
+      "b":"nõude tunnustamisega kohustatud isiku poolt",
+      "c":"läbirääkimiste pidamisega läbirääkimiste toimumise ajaks",
+      "d":"nõude loovutamisega"
+      }
+   },
+   {
+    "question": "Tsiviilasjades võib maakohtu otsuse peale apellatsioonkaebuse esitada",
+    "answers": { 
+        "a":"10 päeva jooksul alates otsuse apellandile kättetoimetamisest",
+        "b":"15 päeva jooksul alates otsuse apellandile kättetoimetamisest",
+        "c":"20 päeva jooksul alates otsuse apellandile kättetoimetamisest",
+        "d":"30 päeva jooksul alates otsuse apelllandile kättetoimetamisest"
+      }
+   },
+    {
+    "question": "Müügilepingu sõlmimisega",
+    "answers": {
+      "a": "tekib ostjal õigus nõuda müügieseme omandi ülekandmist",
+      "b": "läheb müügieseme omand üle ostjale juhul, kui ta on müügihinna täielikult tasunud",
+      "c": "läheb müügieseme omand üle ostjale, välja arvatud juhul, kui on kokku lepitud omandireservatsioonis",
+      "d": "täidab ostja oma ostuhinna tasumise kohustuse"
+    }
+   },{
+    "question": "Käsipant, samuti õiguste pant lõpeb",
+    "answers":  {
+      "a":"pandiga tagatud nõude loovutamisel",
+      "b":"pandi registrist kustutamisel",
+      "c":"pandi eseme üleandmisel",
+      "d":"kui pandipidaja teatab pantijale kirjalikult pandi vabastamisest"
+      }
+     },
+   {
+    "question": "Nõude aegumine peatub mh",
+    "answers": {
+      "a":"hagiavalduse esitamisega",
+      "b":"nõude tunnustamisega kohustatud isiku poolt",
+      "c":"läbirääkimiste pidamisega läbirääkimiste toimumise ajaks",
+      "d":"nõude loovutamisega"
+      }
+   },
+   {
+    "question": "Tsiviilasjades võib maakohtu otsuse peale apellatsioonkaebuse esitada",
+    "answers": { 
+        "a":"10 päeva jooksul alates otsuse apellandile kättetoimetamisest",
+        "b":"15 päeva jooksul alates otsuse apellandile kättetoimetamisest",
+        "c":"20 päeva jooksul alates otsuse apellandile kättetoimetamisest",
+        "d":"30 päeva jooksul alates otsuse apelllandile kättetoimetamisest"
+      }
+   },
+    {
+    "question": "Müügilepingu sõlmimisega",
+    "answers": {
+      "a": "tekib ostjal õigus nõuda müügieseme omandi ülekandmist",
+      "b": "läheb müügieseme omand üle ostjale juhul, kui ta on müügihinna täielikult tasunud",
+      "c": "läheb müügieseme omand üle ostjale, välja arvatud juhul, kui on kokku lepitud omandireservatsioonis",
+      "d": "täidab ostja oma ostuhinna tasumise kohustuse"
+    }
+   },{
+    "field": "Karistusõigus ja kriminaalmenetlus",
     "question": "Käsipant, samuti õiguste pant lõpeb",
     "answers":  {
       "a":"pandiga tagatud nõude loovutamisel",
@@ -135,6 +248,7 @@ function QuestionListCtrl($scope) {
       }
    },
     {
+      "field": "Riigi põhikord, avalik õigus ja halduskohtumenetlus ",
     "question": "Müügilepingu sõlmimisega",
     "answers": {
       "a": "tekib ostjal õigus nõuda müügieseme omandi ülekandmist",
@@ -266,111 +380,7 @@ function QuestionListCtrl($scope) {
       }
    },
    {
-    "question": "Tsiviilasjades võib maakohtu otsuse peale apellatsioonkaebuse esitada",
-    "answers": { 
-        "a":"10 päeva jooksul alates otsuse apellandile kättetoimetamisest",
-        "b":"15 päeva jooksul alates otsuse apellandile kättetoimetamisest",
-        "c":"20 päeva jooksul alates otsuse apellandile kättetoimetamisest",
-        "d":"30 päeva jooksul alates otsuse apelllandile kättetoimetamisest"
-      }
-   },
-    {
-    "question": "Müügilepingu sõlmimisega",
-    "answers": {
-      "a": "tekib ostjal õigus nõuda müügieseme omandi ülekandmist",
-      "b": "läheb müügieseme omand üle ostjale juhul, kui ta on müügihinna täielikult tasunud",
-      "c": "läheb müügieseme omand üle ostjale, välja arvatud juhul, kui on kokku lepitud omandireservatsioonis",
-      "d": "täidab ostja oma ostuhinna tasumise kohustuse"
-    }
-   },{
-    "question": "Käsipant, samuti õiguste pant lõpeb",
-    "answers":  {
-      "a":"pandiga tagatud nõude loovutamisel",
-      "b":"pandi registrist kustutamisel",
-      "c":"pandi eseme üleandmisel",
-      "d":"kui pandipidaja teatab pantijale kirjalikult pandi vabastamisest"
-      }
-     },
-   {
-    "question": "Nõude aegumine peatub mh",
-    "answers": {
-      "a":"hagiavalduse esitamisega",
-      "b":"nõude tunnustamisega kohustatud isiku poolt",
-      "c":"läbirääkimiste pidamisega läbirääkimiste toimumise ajaks",
-      "d":"nõude loovutamisega"
-      }
-   },
-   {
-    "question": "Tsiviilasjades võib maakohtu otsuse peale apellatsioonkaebuse esitada",
-    "answers": { 
-        "a":"10 päeva jooksul alates otsuse apellandile kättetoimetamisest",
-        "b":"15 päeva jooksul alates otsuse apellandile kättetoimetamisest",
-        "c":"20 päeva jooksul alates otsuse apellandile kättetoimetamisest",
-        "d":"30 päeva jooksul alates otsuse apelllandile kättetoimetamisest"
-      }
-   },
-    {
-    "question": "Müügilepingu sõlmimisega",
-    "answers": {
-      "a": "tekib ostjal õigus nõuda müügieseme omandi ülekandmist",
-      "b": "läheb müügieseme omand üle ostjale juhul, kui ta on müügihinna täielikult tasunud",
-      "c": "läheb müügieseme omand üle ostjale, välja arvatud juhul, kui on kokku lepitud omandireservatsioonis",
-      "d": "täidab ostja oma ostuhinna tasumise kohustuse"
-    }
-   },{
-    "question": "Käsipant, samuti õiguste pant lõpeb",
-    "answers":  {
-      "a":"pandiga tagatud nõude loovutamisel",
-      "b":"pandi registrist kustutamisel",
-      "c":"pandi eseme üleandmisel",
-      "d":"kui pandipidaja teatab pantijale kirjalikult pandi vabastamisest"
-      }
-     },
-   {
-    "question": "Nõude aegumine peatub mh",
-    "answers": {
-      "a":"hagiavalduse esitamisega",
-      "b":"nõude tunnustamisega kohustatud isiku poolt",
-      "c":"läbirääkimiste pidamisega läbirääkimiste toimumise ajaks",
-      "d":"nõude loovutamisega"
-      }
-   },
-   {
-    "question": "Tsiviilasjades võib maakohtu otsuse peale apellatsioonkaebuse esitada",
-    "answers": { 
-        "a":"10 päeva jooksul alates otsuse apellandile kättetoimetamisest",
-        "b":"15 päeva jooksul alates otsuse apellandile kättetoimetamisest",
-        "c":"20 päeva jooksul alates otsuse apellandile kättetoimetamisest",
-        "d":"30 päeva jooksul alates otsuse apelllandile kättetoimetamisest"
-      }
-   },
-    {
-    "question": "Müügilepingu sõlmimisega",
-    "answers": {
-      "a": "tekib ostjal õigus nõuda müügieseme omandi ülekandmist",
-      "b": "läheb müügieseme omand üle ostjale juhul, kui ta on müügihinna täielikult tasunud",
-      "c": "läheb müügieseme omand üle ostjale, välja arvatud juhul, kui on kokku lepitud omandireservatsioonis",
-      "d": "täidab ostja oma ostuhinna tasumise kohustuse"
-    }
-   },{
-    "question": "Käsipant, samuti õiguste pant lõpeb",
-    "answers":  {
-      "a":"pandiga tagatud nõude loovutamisel",
-      "b":"pandi registrist kustutamisel",
-      "c":"pandi eseme üleandmisel",
-      "d":"kui pandipidaja teatab pantijale kirjalikult pandi vabastamisest"
-      }
-     },
-   {
-    "question": "Nõude aegumine peatub mh",
-    "answers": {
-      "a":"hagiavalduse esitamisega",
-      "b":"nõude tunnustamisega kohustatud isiku poolt",
-      "c":"läbirääkimiste pidamisega läbirääkimiste toimumise ajaks",
-      "d":"nõude loovutamisega"
-      }
-   },
-   {
+    "field": "Advokatuur ",
     "question": "Tsiviilasjades võib maakohtu otsuse peale apellatsioonkaebuse esitada",
     "answers": { 
         "a":"10 päeva jooksul alates otsuse apellandile kättetoimetamisest",
