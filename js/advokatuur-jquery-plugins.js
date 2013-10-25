c2bad63bbf948f7ed5d970a8c2e65cacacc3d438
Gert Glükmann <gert.glukmann@netgroup.ee>
2013-10-25 16:54:31 +0300

    muudatused

--- js/advokatuur-jquery-plugins.js
+++ js/advokatuur-jquery-plugins.js
@@ -83,18 +83,9 @@ $(document).ready(function() {
          limit: 3,
          source: [
           { value: 0, text: 'Vali' },
-          { value: 1, text: '1' },
-          { value: 2, text: '2' },
-          { value: 3, text: '3' },
-          { value: 4, text: '4' },
-          { value: 5, text: '5' },
-          { value: 6, text: '6' },
-          { value: 7, text: '7' },
-          { value: 8, text: '8' },
-          { value: 9, text: '9' },
-          { value: 10, text: '10' },
-          { value: 11, text: 'Mitte ilmunud' },
-          { value: 12, text: 'Kõrvaldatud' }
+          { value: 1, text: 'Arvestatud' },
+          { value: 3, text: 'Mitte ilmunud' },
+          { value: 4, text: 'Kõrvaldatud' }
          ]
      });
  });
