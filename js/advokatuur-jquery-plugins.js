272fbd4e3fb9cbf33af8085da7a72bc5d123f7ce
Gert Glükmann <gert.glukmann@netgroup.ee>
2013-11-01 17:02:10 +0200

    reede ohtu

--- js/advokatuur-jquery-plugins.js
+++ js/advokatuur-jquery-plugins.js
@@ -82,7 +82,7 @@ $(document).ready(function() {
          pk: 1,
          limit: 3,
          source: [
-          { value: 0, text: 'Vali' },
+          
           { value: 1, text: 'Arvestatud' },
           { value: 3, text: 'Mitte ilmunud' },
           { value: 4, text: 'Kõrvaldatud' }
@@ -90,13 +90,14 @@ $(document).ready(function() {
      });
  });
 
+// START: xeditable aadress ehk toimumiskoha lisamine
  $(function () {
-     $('.toimumiskoht').editable({
+     $('.js-toimumiskoht').editable({
          url: '/post',
          title: 'Sisesta toimumiskoht ja kohtade arv #',
          value: {
-             toimumiskoht: "Rävala pst 3, Tallinn",
-             kohti: "15"
+             street: "Rävala pst 3, Tallinn",
+             building: ""
          }
      });
  });
@@ -111,57 +112,23 @@ $(document).ready(function() {
      $.fn.editableutils.inherit(Address, $.fn.editabletypes.abstractinput);
 
      $.extend(Address.prototype, {
-         /**
-         Renders input from tpl
- 
-         @method render() 
-         **/
          render: function () {
              this.$input = this.$tpl.find('input');
          },
 
-         /**
-         Default method to show value in element. Can be overwritten by display option.
-         
-         @method value2html(value, element) 
-         **/
          value2html: function (value, element) {
              if (!value) {
                  $(element).empty();
                  return;
              }
-             var html = $('<div>').text(value.city).html() + ', ' + $('<div>').text(value.street).html() + ' st., bld. ' + $('<div>').text(value.building).html();
+             var html = '<i class="icon-plus-sign"></i>';
              $(element).html(html);
          },
 
-         /**
-         Gets value from element's html
-         
-         @method html2value(html) 
-         **/
          html2value: function (html) {
-             /*
-               you may write parsing method to get value by element's html
-               e.g. "Moscow, st. Lenina, bld. 15" => {city: "Moscow", street: "Lenina", building: "15"}
-               but for complex structures it's not recommended.
-               Better set value directly via javascript, e.g. 
-               editable({
-                   value: {
-                       city: "Moscow", 
-                       street: "Lenina", 
-                       building: "15"
-                   }
-               });
-             */
              return null;
          },
 
-         /**
-          Converts value to string. 
-          It is used in internal comparing (not for sending to server).
-          
-          @method value2str(value)  
-         **/
          value2str: function (value) {
              var str = '';
              if (value) {
@@ -172,61 +139,29 @@ $(document).ready(function() {
              return str;
          },
 
-         /*
-          Converts string to value. Used for reading value from 'data-value' attribute.
-          
-          @method str2value(str)  
-         */
          str2value: function (str) {
-             /*
-             this is mainly for parsing value defined in data-value attribute. 
-             If you will always set value by javascript, no need to overwrite it
-             */
              return str;
          },
 
-         /**
-          Sets value of input.
-          
-          @method value2input(value) 
-          @param {mixed} value
-         **/
          value2input: function (value) {
              if (!value) {
                  return;
              }
-             this.$input.filter('[name="city"]').val(value.city);
              this.$input.filter('[name="street"]').val(value.street);
              this.$input.filter('[name="building"]').val(value.building);
          },
 
-         /**
-          Returns value of input.
-          
-          @method input2value() 
-         **/
          input2value: function () {
              return {
-                 city: this.$input.filter('[name="city"]').val(),
                  street: this.$input.filter('[name="street"]').val(),
                  building: this.$input.filter('[name="building"]').val()
              };
          },
 
-         /**
-         Activates input: sets focus on the first field.
-         
-         @method activate() 
-        **/
          activate: function () {
-             this.$input.filter('[name="city"]').focus();
+             this.$input.filter('[name="street"]').focus();
          },
 
-         /**
-          Attaches handler to submit form in case of 'showbuttons=false' mode
-          
-          @method autosubmit() 
-         **/
          autosubmit: function () {
              this.$input.keydown(function (e) {
                  if (e.which === 13) {
@@ -237,11 +172,11 @@ $(document).ready(function() {
      });
 
      Address.defaults = $.extend({}, $.fn.editabletypes.abstractinput.defaults, {
-         tpl: '<div class="editable-address"><label><span>Toimumiskoht: </span><select>' +
+         tpl: '<div class="editable-address"><label><span>Toimumiskoht: </span><select name="street">' +
                                                                                  '<option>Rävala pst 3, Tallinn</option>' +
                                                                                  '<option>Rävala pst 3, Tallinn</option>' +
                                                                                 '</select></label></div>' +
-              '<div class="editable-address"><label><span>Kohtade arv: </span><input type="text" name="street" class="input-small"></label></div>',
+              '<div class="editable-address"><label><span>Kohtade arv: </span><input type="text" name="building" class="input-small"></label></div>',
 
          inputclass: ''
      });
@@ -249,6 +184,8 @@ $(document).ready(function() {
      $.fn.editabletypes.address = Address;
 
  }(window.jQuery));
+// END: xeditable aadress ehk toimumiskoha lisamine
+
 $('table').on('click', '.icon-trash', function(e){
 
         $(this).closest('tr').remove()
